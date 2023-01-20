2fba3558bec2d4416e0619ded3948dba5f7fb170
David Aguina <aguinadavid@gmail.com>
2023-01-20 10:55:11 -0300

    um debouncer mais elegante

---
+++ src/components/GithubRepo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-autocomplete
+        v-model="user"
+        :items="userlist"
+        :loading="userloading"
+        :search-input.sync="usersearch"
+        item-text="login"
+    />
+  </div>
+</template>
+
+<script>
+
+  import {debouncerdecorator} from '@/helpers/debouncer.js'
+
+  export default {
+    data: () => ({
+      user: null,
+      usersearch: null,
+      userlist: [],
+      userloading: false,
+    }),
+    methods: {
+      procuraUsuarioGithub: debouncerdecorator(() => { // atenção: não use () => {} aqui. vai quebrar o decorator
+        console.log('faz de conta que eu perguntei pro github: ' + this.usersearch)
+        this.userloading = true
+        setTimeout(() => {
+          this.userlist = [
+          {login: 'joao'},
+          {login: 'jose'}
+          ]
+          this.userloading = false
+        }, 1000)
+      }, 500)
+    },
+    watch: {
+      usersearch() {
+        this.procuraUsuariosGithub()
+
+      }
+    }
+  }
+</script>
+    
